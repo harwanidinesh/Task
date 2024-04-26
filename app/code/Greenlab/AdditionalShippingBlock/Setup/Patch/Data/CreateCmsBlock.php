226e1209d402bf732518e7e289c6ba52011f3bf6
dharwani09 <dharwani@southco.com>
2024-04-26 18:48:22 +0530

    cms block on checkout page

---
+++ app/code/Greenlab/AdditionalShippingBlock/Setup/Patch/Data/CreateCmsBlock.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Greenlab\AdditionalShippingBlock\Setup\Patch\Data;
+
+use Magento\Cms\Model\BlockFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Store\Model\Store;
+
+class CreateCmsBlock implements DataPatchInterface, PatchRevertableInterface
+{
+    const CMS_BLOCK_IDENTIFIER = 'shipping_method_cms_block';
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var BlockFactory
+     */
+    private $blockFactory;
+
+    /**
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param BlockFactory $blockFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        BlockFactory $blockFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->blockFactory = $blockFactory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->startSetup();
+
+        $this->blockFactory->create()
+            ->setTitle('Shipping CMS Block')
+            ->setIdentifier(self::CMS_BLOCK_IDENTIFIER)
+            ->setIsActive(true)
+            ->setContent('<div class="additional-block"><div class="step-title" >Additional Block</div><div>Sample Block After Shipping Method Block Content</div></div>')
+            ->setStores([Store::DEFAULT_STORE_ID])
+            ->save();
+
+        $this->moduleDataSetup->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function revert()
+    {
+        $sampleCmsBlock = $this->blockFactory
+            ->create()
+            ->load(self::CMS_BLOCK_IDENTIFIER, 'identifier');
+
+        if ($sampleCmsBlock->getId()) {
+            $sampleCmsBlock->delete();
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+}
+
---
+++ app/code/Greenlab/Sidebar/Model/CmsConfigProvider.php
@@ -0,0 +1,48 @@
+<?php
+namespace Greenlab\Sidebar\Model;
+
+use \Magento\Checkout\Model\ConfigProviderInterface;
+use Magento\Framework\View\LayoutInterface;
+use \Magento\Store\Model\StoreManagerInterface;
+
+/**
+* Class ConfigProvider
+* @codeCoverageIgnore
+*/
+final class CmsConfigProvider implements ConfigProviderInterface
+{
+
+private $layout;
+private $storeManager;
+private $cmsBlock;
+
+public function __construct(LayoutInterface $layout, StoreManagerInterface $storeManager, $blockId)
+{
+$this->layout = $layout;
+$this->storeManager = $storeManager;
+$this->cmsBlock = $blockId;
+}
+
+public function getStoreId()
+{
+return $this->storeManager->getStore()->getId();
+}
+
+public function constructBlock($blockId)
+{
+$storeId = $this->getStoreId();
+$block = $this->layout->createBlock('Magento\Cms\Block\Block')
+->setBlockId($blockId)->setStoreId($storeId)->toHtml();
+return $block;
+}
+
+public function getConfig()
+{
+$cmsBlock = '';
+$blockId = $this->cmsBlock;
+if (isset($blockId) && $blockId != '') {
+$cmsBlock = $this->constructBlock($blockId);
+}
+return ['cms_block' => $cmsBlock];
+}
+}
