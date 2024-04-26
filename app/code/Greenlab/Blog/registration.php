7f95e7bcd0e33624995620f33c5a546dadcc67cc
dharwani09 <dharwani@southco.com>
2024-04-26 18:26:16 +0530

    Revert "app code folder and composer.json and composer.lock file"
    This reverts commit d1f76046b91e1d7582ab312fd4d17081389a0660.

--- app/code/Greenlab/Blog/registration.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Greenlab_Blog',
-    __DIR__
-);
--- app/code/Greenlab/Sidebar/registration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-\Magento\Framework\Component\ComponentRegistrar::register(
-\Magento\Framework\Component\ComponentRegistrar::MODULE,
-'Greenlab_Sidebar',
-__DIR__
-);
